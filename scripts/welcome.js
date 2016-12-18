e2bc0d180d3c7f38b60e4ca1eb1802c273a8918a
midhruvjaink <blivefoundations@gmail.com>
2016-12-18 09:34:07 +0530

    meta and analytics update

--- scripts/welcome.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    var burger = document.querySelector('.burger-container'),
-        header = document.querySelector('.header');
-
-    burger.onclick = function() {
-        header.classList.toggle('menu-opened');
-    }
-}());
