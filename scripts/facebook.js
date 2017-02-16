1dfd11f53cc98cc59e1ce5e2e5ba8070af0d36e6
midhruvjaink <blivefoundations@gmail.com>
2017-02-16 20:10:56 +0530

    user videos data

---
+++ scripts/facebook.js
@@ -0,0 +1,53 @@
+userId = "none";
+
+/*facebook login*/
+window.fbAsyncInit = function() {
+    FB.init({
+        appId: '758134654325447',
+        xfbml: true,
+        version: 'v2.5'
+    });
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+            $("#login").css("display", "none");
+            getInfo();
+        } else if (response.status === 'not_authorized') {
+
+        } else {
+
+        }
+    });
+};
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function login() {
+    FB.login(function(response) {
+        if (response.status === 'connected') {
+            $("#login").css("display", "none");
+            getInfo();
+        } else if (response.status === 'not_authorized') {
+
+        } else {
+
+        }
+    });
+}
+
+function getInfo() {
+  FB.api('/me', 'GET', {fields: 'first_name,email,last_name,name,id,picture.width(150).height(150)'}, function(response) {
+  photoURL = response.picture.data.url;
+  $("#avatar").attr("src", photoURL);
+  $("#userName").html(response.first_name);
+  window.userId = response.id;
+  db_userInfo(response.id, response.name, response.email, photoURL, response.first_name);
+  });
+}
--- scripts/script.js
+++ scripts/script.js
@@ -12,55 +12,8 @@ var config = {
 firebase.initializeApp(config);
 dbref = firebase.database().ref();
 
-/*facebook login*/
-window.fbAsyncInit = function() {
-    FB.init({
-        appId: '758134654325447',
-        xfbml: true,
-        version: 'v2.5'
-    });
-    FB.getLoginStatus(function(response) {
-        if (response.status === 'connected') {
-            $("#login").css("display", "none");
-            getInfo();
-        } else if (response.status === 'not_authorized') {
-
-        } else {
-
-        }
-    });
-};
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-        return;
-    }
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-function login() {
-    FB.login(function(response) {
-        if (response.status === 'connected') {
-            $("#login").css("display", "none");
-            getInfo();
-        } else if (response.status === 'not_authorized') {
-
-        } else {
-
-        }
-    });
-}
-
-function getInfo() {
-  FB.api('/me', 'GET', {fields: 'first_name,email,last_name,name,id,picture.width(150).height(150)'}, function(response) {
-  photoURL = response.picture.data.url;
-  $("#avatar").attr("src", photoURL);
-  $("#userName").html(response.first_name);
-  db_userInfo(response.id, response.name, response.email, photoURL, response.first_name);
-  });
+function db_user_videos(id, title){
+  dbref.child("users").child(userId).child("videos").child(id).set(title);
 }
 
 function db_userInfo(id, name, email, photoURL, first_name){
